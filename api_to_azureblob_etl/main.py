d3754af5e1002342ddcaca7edc8254b83321649e
Armiel <armielgonzales@gmail.com>
2024-05-14 01:05:03 +0800

    moved main.py to root directory

--- api_to_azureblob_etl/main.py
+++ api_to_azureblob_etl/main.py
@@ -1,9 +1,3 @@
-import sys, os
-
-# This line will enable us to import python scripts from other folders
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-
-# Import pipelines and datetime library
 from pipeline.nba_games_pipeline import nba_games_pipeline
 from pipeline.upload_to_blob_pipeline import azure_container_pipeline
 from datetime import date
